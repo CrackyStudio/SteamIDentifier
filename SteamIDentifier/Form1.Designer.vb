ce9a2172b6062583c18273285a20dce4ad63e863
Antoine Nivoy <antoine.nivoy@gmail.com>
2018-05-18 14:49:26 +0200

    Add files via upload

---
+++ SteamIDentifier/Form1.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(284, 34)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(381, 24)
+        Me.Button1.TabIndex = 0
+        Me.Button1.TabStop = False
+        Me.Button1.Text = "Identify"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(284, 8)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(381, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.TabStop = False
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(284, 74)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(381, 29)
+        Me.Label1.TabIndex = 3
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(284, 105)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(381, 29)
+        Me.Label2.TabIndex = 4
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label3.Location = New System.Drawing.Point(425, 256)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(96, 15)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Powered by Steam"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label4
+        '
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label4.Location = New System.Drawing.Point(281, 265)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(384, 47)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "SteamIDentifier and Cracky are not affiliated with Valve. All trademarks and regi" &
+    "stered trademarks are the property of their respective owners. "
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.DarkGray
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(250, 250)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 8
+        Me.PictureBox1.TabStop = False
+        '
+        'Label5
+        '
+        Me.Label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(8, 274)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(258, 29)
+        Me.Label5.TabIndex = 9
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label6
+        '
+        Me.Label6.BackColor = System.Drawing.Color.DarkGray
+        Me.Label6.Location = New System.Drawing.Point(8, 8)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(258, 258)
+        Me.Label6.TabIndex = 10
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(284, 153)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(320, 87)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 11
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(610, 153)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(55, 87)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox3.TabIndex = 12
+        Me.PictureBox3.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(677, 309)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label4)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "SteamIDentifier"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+End Class
---
+++ SteamIDentifier/Form1.vb
@@ -0,0 +1,131 @@
+﻿'_________                       __
+'\_   ___ \____________    ____ |  | _____.__.
+'/    \  \/\_  __ \__  \ _/ ___\|  |/ <   |  |
+'\     \____|  | \// __ \\  \___|    < \___  |
+' \______  /|__|  (____  /\___  >__|_ \/ ____|
+'        \/            \/     \/     \/\/  
+'This freeware was developed by Cracky
+'http://steamcommunity.com/id/officialcracky/
+'This code is free to use - If you use it, give credits
+Imports System.Runtime.InteropServices
+Public Class Form1
+    <DllImport("user32.dll", CharSet:=CharSet.Auto)>
+    Private Shared Function SendMessage(ByVal hWnd As IntPtr, ByVal msg As Integer, ByVal wParam As Integer, <MarshalAs(UnmanagedType.LPWStr)> ByVal lParam As String) As Int32
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SendMessage(Me.TextBox1.Handle, &H1501, 0, "Put URL here")
+        If My.Computer.Network.IsAvailable Then
+        Else
+            MessageBox.Show("Internet connection is not detected!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Me.Close()
+        End If
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Label1.Text = ""
+        Me.Label2.Text = ""
+        If TextBox1.Text.Length > 26 Then
+            Dim sourceString As String = New System.Net.WebClient().DownloadString(TextBox1.Text)
+            Dim filePath As String
+            filePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+            My.Computer.FileSystem.WriteAllText(filePath & "SteamID.txt", sourceString, False)
+            For Each k As String In IO.File.ReadLines(filePath & "SteamID.txt")
+                If k.Contains("g_rgProfileData =") Then
+                    Dim mystr As String = IO.File.ReadAllLines(filePath & "SteamID.txt").FirstOrDefault(Function(x) x.Contains("g_rgProfileData"))
+                    Dim cut_at As String = """steamid"":"""
+                    Dim cut_at2 As String = """,""personname"""
+                    Dim xev As Integer = InStr(mystr, cut_at)
+                    Dim string_before As String = mystr.Substring(0, xev - 2)
+                    Dim string_after As String = mystr.Substring(xev + cut_at2.Length - 4)
+                    Label1.Text = string_after.Substring(0, 17)
+                End If
+            Next
+            Dim Valeur As Int64 = 0
+            Dim Resultat As Double = 0
+            If Not (String.IsNullOrEmpty(Me.Label1.Text)) Then
+                If (Int64.TryParse(Me.Label1.Text, Valeur)) Then
+                    Resultat = ((Valeur + 0) - 76561197960265728) / 2
+                    If Valeur Mod 2 = 0 Then
+                        Me.Label2.Text = "STEAM_0:0:" & Resultat.ToString
+                    Else
+                        Me.Label2.Text = "STEAM_0:1:" & Resultat.ToString(0) - 1
+                    End If
+                End If
+            End If
+            For Each l As String In IO.File.ReadLines(filePath & "SteamID.txt")
+                If l.Contains("<div class=""playerAvatarAutoSizeInner""><img src=""") Then
+                    Dim mystr As String = IO.File.ReadAllLines(filePath & "SteamID.txt").FirstOrDefault(Function(x) x.Contains("<div class=""playerAvatarAutoSizeInner""><img src="""))
+                    Dim cut_at As String = "src="""
+                    Dim cut_at2 As String = """,""personname"""
+                    Dim xev As Integer = InStr(mystr, cut_at)
+                    Dim string_after As String = mystr.Substring(xev + cut_at2.Length - 10)
+                    PictureBox1.Image = New System.Drawing.Bitmap(New IO.MemoryStream(New System.Net.WebClient().DownloadData(string_after.Substring(0, string_after.Length - 8))))
+                End If
+            Next
+            For Each m As String In IO.File.ReadLines(filePath & "SteamID.txt")
+                If m.Contains("<div class=""playerAvatar profile_header_size online") Then
+                    Label6.BackColor = Color.LightSkyBlue
+                ElseIf m.Contains("<div class=""playerAvatar profile_header_size in-game") Then
+                    Label6.BackColor = Color.LimeGreen
+                ElseIf m.Contains("<div class=""playerAvatar profile_header_size offline") Then
+                    Label6.BackColor = Color.DarkGray
+                End If
+            Next
+            For Each n As String In IO.File.ReadLines(filePath & "SteamID.txt")
+                If n.Contains("<span class=""actual_persona_name"">") Then
+                    Dim mystr As String = IO.File.ReadAllLines(filePath & "SteamID.txt").FirstOrDefault(Function(x) x.Contains("<span class=""actual_persona_name"">"))
+                    Dim cut_at As String = "name"">"
+                    Dim cut_at2 As String = """,""personname"""
+                    Dim xev As Integer = InStr(mystr, cut_at)
+                    Dim string_after As String = mystr.Substring(xev + cut_at2.Length - 10)
+                    Label5.Text = string_after.Substring(1, string_after.Length - 8)
+                End If
+            Next
+        Else
+            Me.Label1.Text = ""
+            Me.Label2.Text = ""
+            MessageBox.Show("Invalid link!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+        Dim webAddress As String = "http://store.steampowered.com/"
+        Process.Start(webAddress)
+    End Sub
+    Private Sub LabelMouseEnter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label3.MouseEnter
+        Dim newFont As New Font(Label3.Font.Name, Label3.Font.Size, FontStyle.Underline)
+        Label3.Font = newFont
+    End Sub
+    Private Sub LabelMouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.MouseLeave, Label2.MouseLeave, Label3.MouseLeave
+        Dim newFont2 As New Font(Label3.Font.Name, Label3.Font.Size, FontStyle.Regular)
+        Label3.Font = newFont2
+    End Sub
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+        If Label1.Text <> "" Then
+            Clipboard.SetText(Label1.Text)
+        End If
+    End Sub
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+        If Label2.Text <> "" Then
+            Clipboard.SetText(Label2.Text)
+        End If
+    End Sub
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+        If Label5.Text <> "" Then
+            Clipboard.SetText(Label5.Text)
+        End If
+    End Sub
+    Private Sub TextBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.Click
+        Me.TextBox1.Text = ""
+    End Sub
+    Private Sub Form1_Closing(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles MyBase.Closing
+        'Donate link: paypal.me/officialcracky
+    End Sub
+End Class
+'  _________ __
+' /   _____//  |_  ____ _____    _____  
+' \_____  \\   __\/ __ \\__  \  /     \ 
+' /        \|  | \  ___/ / __ \|  Y Y  \
+'/_______  /|__|  \___  >____  /__|_|  /
+'        \/           \/     \/      \/      
+'http://store.steampowered.com/
+'Powered by Steam
+'SteamIDentifier and Cracky are not affiliated with Valve. All trademarks and registered trademarks are the property of their respective owners. 
